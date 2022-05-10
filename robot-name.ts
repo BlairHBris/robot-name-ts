1f17fe5aec2c0ae4a43e708c7c09816eabb2735a
Blair Bristow <blairhbristow@gmail.com>
2022-05-10 16:36:59 -0400

    Add kyle session

--- robot-name.ts
+++ robot-name.ts
@@ -1,6 +1,54 @@
+interface IRobot {
+    constructor: () => void
+
+}
+
 export class Robot {
     name: string
+    static usedNames: string[] = []
+    static releaseNames() {
+        return null
+    }
+
     constructor() {
-        this.name = 'Me'
+        this.name = this.uniqueRandomName
+    }
+
+    resetName() {
+        this.name = this.uniqueRandomName
+    }
+
+    private get uniqueRandomName() {
+        let newName
+        do {
+            newName = this.randomName
+        } while (Robot.usedNames.includes(newName))
+        Robot.usedNames.push(newName)
+        return newName
+    }
+
+    private get randomName() {
+        return `${this.twoRandomLetters}${this.random3digits}`
+    }
+
+    private get twoRandomLetters() {
+        const letter1 = this.randomLetter
+        const letter2 = this.randomLetter
+
+        return `${letter1}${letter2}`
+    }
+
+    private get random3digits() {
+        return `${this.randomNumber0to9}${this.randomNumber0to9}${this.randomNumber0to9}`
+    }
+
+    private get randomLetter() {
+        const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        const randomNumber = Math.floor(Math.random() * 26)
+        return letters[randomNumber]
+    }
+
+    private get randomNumber0to9 () {
+        return Math.floor(Math.random() * 10)
     }
 }
